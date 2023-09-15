fd8543cc1607fc4ef04ffe1ae081593e4cc998a7
Mahboob-Alam <iammahboob.a@gmail.com>
2023-09-15 17:37:02 +0530

    14. Basic Authentication and Permisson Classes

---
+++ 14 Basic auth and permission class/basic_auth_permission/api/urls.py
@@ -0,0 +1,20 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import StudentModelViewSet, StudentReadOnlyModelViewSet # StudentViewSetAPI
+
+router = DefaultRouter()
+
+# router.register('studentapi', StudentViewSetAPI,  basename='studentapi')   # using viewset 
+
+# no need to add slash after the url path 
+router.register('crud/studentapi', StudentModelViewSet,  basename='crud_studentapi')   # using modelviewset 
+router.register('list/studentapi', StudentReadOnlyModelViewSet,  basename='read_only_studentapi')   # using readonlymodelviewset 
+
+
+urlpatterns = [
+
+        path('', include(router.urls)),
+]
+
---
+++ 14 Basic auth and permission class/basic_auth_permission/api/views.py
@@ -0,0 +1,132 @@
+
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.core.exceptions import ObjectDoesNotExist
+
+
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+
+
+from .models import Student
+from .serializers import StudentSerializer
+
+
+
+
+'''
+Full CRUD is implamented here.
+ModelViewSet inherits GenericAPIView. 
+ModelViewSet also handles try catch / edge cases 
+'''
+'''
+14. Authentication and Permission Classes. 
+'''
+class StudentModelViewSet(viewsets.ModelViewSet): 
+        queryset = Student.objects.all()
+        serializer_class = StudentSerializer
+        
+        # We can individually use auth and permission classes 
+        # If global auth and permisson classes is defined, then the view is auto applied the global auth and permission settings 
+        # Or we can override the global auth and permission setting setting auth and permission locally. 
+        # See the default auth and permission classes settings in settings.py 
+        authentication_classes = [BasicAuthentication]
+        permission_classes = [IsAuthenticated]
+        # permission_classes = [AllowAny]
+        
+        
+        
+        
+'''
+ReadOnlyModelViewSet only provides List and Retrieve (only provides GET method.)
+ReadOnlyModelViewSet also inherits GenericAPIView 
+'''
+class StudentReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet): 
+        queryset = Student.objects.all()
+        serializer_class = StudentSerializer 
+        
+        
+
+
+
+
+
+
+
+
+''' Below Code Is Using ViewSet '''
+
+'''
+
+list
+create
+retrieve 
+update
+partial_update 
+destroy 
+
+
+
+class StudentViewSetAPI(viewsets.ViewSet): 
+        
+        def list(self, request): 
+                students = Student.objects.all()
+                serializer = StudentSerializer(students, many=True)
+                return Response({'data' : serializer.data}, status=status.HTTP_200_OK)
+                # return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        def create(self, request): 
+                serializer = StudentSerializer(data=request.data)
+                if serializer.is_valid(): 
+                        serializer.save()
+                        return Response({'data' : serializer.data}, status=status.HTTP_201_CREATED)
+                return Response({'error' : serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+        def retrieve(self, request, pk=None): 
+                try: 
+                        student = Student.objects.get(pk=pk)
+                        serializer = StudentSerializer(student)
+                        return Response({'data' : serializer.data}, status=status.HTTP_200_OK)
+                except ObjectDoesNotExist: 
+                        return Response({'error' : f'The ID {pk} Is Invalid !!'}, status=status.HTTP_400_BAD_REQUEST)
+                
+        def update(self, request, pk=None): 
+                try: 
+                        student = Student.objects.get(pk=pk)
+                        serializer = StudentSerializer(student, data=request.data)
+                        if serializer.is_valid(): 
+                                serializer.save()
+                                return Response({'data' : serializer.data}, status=status.HTTP_200_OK)
+                        return Response({'error' : serializer.errors}, status=status.HTTP_304_NOT_MODIFIED)
+                
+                except ObjectDoesNotExist: 
+                        return Response({'error' : f'The ID {pk} Is Invalid !!'}, status=status.HTTP_400_BAD_REQUEST)
+                
+                
+        def partial_update(self, request, pk=None): 
+                try: 
+                        student = Student.objects.get(pk=pk)
+                        serializer = StudentSerializer(student, data=request.data, partial=True)
+                        if serializer.is_valid(): 
+                                serializer.save()
+                                return Response({'data' : serializer.data}, status=status.HTTP_200_OK)
+                        return Response({'error' : serializer.errors}, status=status.HTTP_304_NOT_MODIFIED)
+                
+                except ObjectDoesNotExist: 
+                        return Response({'error' : f'The ID {pk} Is Invalid !!'}, status=status.HTTP_400_BAD_REQUEST)
+                        
+        
+        def destroy(self, request, pk=None): 
+                try: 
+                        student = Student.objects.get(pk=pk)
+                        name = student.name 
+                        student.delete()
+                        return Response({'data' : f'The Student - {name.upper} With ID {pk} Is Deleted !!'}, status=status.HTTP_204_NO_CONTENT)     
+                except ObjectDoesNotExist:    
+                        return Response({'error' : f'The ID {pk} Is Invalid !!'}, status=status.HTTP_400_BAD_REQUEST)
+                        
+                        
+'''
